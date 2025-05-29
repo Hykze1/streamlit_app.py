86ce2c80c58dc535930bb7fbfafe7639198351c2
Ikechukwu  Osuya <100960483+Hykze1@users.noreply.github.com>
2025-05-29 17:21:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Install Chrome
+# Optional: install Chrome for Selenium if needed
 apt-get update
 apt-get install -y wget unzip xvfb libxi6 libgconf-2-4 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome*.deb; apt-get -fy install
 
-# Start your app
-streamlit run app.py
+# Run Streamlit on the Render-assigned port
+streamlit run app.py --server.port=$PORT --server.enableCORS=false
